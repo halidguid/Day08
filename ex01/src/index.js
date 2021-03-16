cf24051ecdb10a4f325163b9f914c5e77b47dd70
halidguid <halidhosi@gmail.com>
2021-03-16 09:10:28 +0100

    Day08

---
+++ ex01/src/index.js
@@ -0,0 +1,10 @@
+function reusableFunction() {
+    console.log("Hi Padawan!");
+}
+
+//write code below this line
+
+reusableFunction();
+//write code above this line
+
+module.exports = reusableFunction;
---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+function functionPrintInfo(name, age) {
+    console.log(name, age); 
+          
+    }
+// Write code below this line
+functionPrintInfo("John", 23);
+functionPrintInfo("Michael", 43);
+// Write code above this line 
+
+module.exports = functionPrintInfo;
